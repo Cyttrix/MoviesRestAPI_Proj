65614fd2fb4bdb0ec586fd1fecd771e861a4a57d
Ari Meyer <ari.f.meyer@gmail.com>
2023-09-18 14:38:35 -0500

    Initillization

---
+++ MoviesWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesWebAPI.Data;
+using MoviesWebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoviesWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var movies = _context.Movies.ToList();
+            return Ok(movies);
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return StatusCode(201, movie);
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Movie film)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            movie.Title = film.Title;
+            movie.RunningTime = film.RunningTime;
+            movie.Genre = film.Genre;
+            _context.Movies.Update(movie);
+            _context.SaveChanges();
+            return Ok(movie);
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+ }
+
+
---
+++ MoviesWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWebAPI.Models;
+
+namespace MoviesWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        { 
+        
+        }
+    }
+}
